b1fe0162cdb011334cee28eb343aef979b9f4611
Uzair Ali <uzairali245@gmail.com>
2023-04-11 07:00:26 -0400

    api for stocks (mockup)

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,8 +1,32 @@
-import React from "react";
-const Home = () => {
-    return(
-        <h2>Home page</h2>
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-    )
-}
-export default Home
+const StockData = () => {
+  const [symbol, setSymbol] = useState('TSLA');
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=4AXW5TFHX9UKEMWT.Y`
+      );
+      setData(response.data['Global Quote']);
+    };
+    fetchData();
+  }, [symbol]);
+
+  return (
+    <div>
+    <h1>Dashboard</h1>
+      <h3>Stock Data for {symbol}</h3>
+      <p>Open: {data['02. open']}</p>
+      <p>High: {data['03. high']}</p>
+      <p>Low: {data['04. low']}</p>
+      <p>Price: {data['05. price']}</p>
+      <p>Volume: {data['06. volume']}</p>
+      <p>Last trading day: {data['07. latest trading day']}</p>
+    </div>
+  );
+};
+
+export default StockData;
